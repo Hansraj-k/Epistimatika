b2a6013fcbe6e85fe7dbf065e66cc8ca458f0b9a
Hansraj <82136657+Hansraj-k@users.noreply.github.com>
2023-08-29 19:47:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,8 @@
-/*===== MENU SHOW Y HIDDEN =====*/
-const navMenu = document.getElementById('nav-menu'),
-    toggleMenu = document.getElementById('nav-toggle'),
-    closeMenu = document.getElementById('nav-close')
-
-// SHOW
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show')
-})
-
-// HIDDEN
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show')
-})
-
-/*===== MOUSEMOVE HOME IMG =====*/
-document.addEventListener('mousemove', move);
-function move(e){
-    this.querySelectorAll('.move').forEach(layer =>{
-        const speed = layer.getAttribute('data-speed')
-
-        const x = (window.innerWidth - e.pageX*speed)/120
-        const y = (window.innerHeight - e.pageY*speed)/120
-
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-    })
-}
-
-/*===== GSAP ANIMATION =====*/
-// NAV
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
-gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
-
-// HOME
-gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+(function($){"use strict";$('.validate-input .input100').each(function(){$(this).on('blur',function(){if(validate(this)==false){showValidate(this);}
+else{$(this).parent().addClass('true-validate');}})})
+var input=$('.validate-input .input100');$('.validate-form').on('submit',function(){var check=true;for(var i=0;i<input.length;i++){if(validate(input[i])==false){showValidate(input[i]);check=false;}}
+return check;});$('.validate-form .input100').each(function(){$(this).focus(function(){hideValidate(this);$(this).parent().removeClass('true-validate');});});function validate(input){if($(input).attr('type')=='email'||$(input).attr('name')=='email'){if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/)==null){return false;}}
+else{if($(input).val().trim()==''){return false;}}}
+function showValidate(input){var thisAlert=$(input).parent();$(thisAlert).addClass('alert-validate');$(thisAlert).append('<span class="btn-hide-validate">&#xf135;</span>')
+$('.btn-hide-validate').each(function(){$(this).on('click',function(){hideValidate(this);});});}
+function hideValidate(input){var thisAlert=$(input).parent();$(thisAlert).removeClass('alert-validate');$(thisAlert).find('.btn-hide-validate').remove();}})(jQuery);
